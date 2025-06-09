573115f43b801e482ea511de45ae4088b525600b
DanielTardelli <danieltardelli16@gmail.com>
2025-06-10 00:07:01 +1000

    changed helper script

--- .kind/create-cluster.sh
+++ .kind/create-cluster.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 kind create cluster --name test-cluster --config /workspaces/k8/.kind/kind-config.yaml \
-&& kubectl config set-context test-cluster \
-&& kubectl config set-cluster test-cluster --server https://host.docker.internal:45355
+&& kubectl config set-context test-cluster
+
+echo "Enter the ephemeral port mapped to the kind cluster container:"
+read port
+
+kubectl config set-cluster test-cluster --server https://host.docker.internal:$port
 
 echo "Hooray"
